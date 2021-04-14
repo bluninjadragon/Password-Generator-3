df91fb4603df8919025789c12217560b6a0633e2
bluninjadragon <jwhbootcamp@gmail.com>
2021-04-14 00:04:59 -0500

    added a few variables to get started

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,29 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+// points variable to the button "generate password"
+let generateBtn = document.querySelector("#generate");
+
+//create array that stores final password
+
+let password = []
+
+
+// stores user response to: 1.) create a password 2.) length of password
+// 3.) lower case, upper case, numeric, and/or special characters
+
+//prompt to create password
+let createPass = prompt("Would you like to create a new password?", "Yes/No?")
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
+}
+
+function generatePassword(); {
+  
 }
 
 // Add event listener to generate button
