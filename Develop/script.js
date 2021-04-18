a1e38d34b13b811f422e88885db2dffbd3c20117
bluninjadragon <jwhbootcamp@gmail.com>
2021-04-18 18:57:48 -0500

    added prompt functions

--- Develop/script.js
+++ Develop/script.js
@@ -4,23 +4,38 @@
 let generateBtn = document.querySelector("#generate");
 //create array that stores final password
 
-let lowerCase = 'abcdefghijklmnopqrstuvwxyz0123456789'.split(
-  ''
-);
-let upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(
-  ''
-);
-let specialChars = ' !\"#$%&\'()*+,-./:;<=>?@[]^_`{|}~'.split(
-  ''
-);
-let allChars = lowerCase.concat(upperCase).concat(specialChars);
+let lowerCase = []
+let upperCase = []
+let specialChar = []
+let allChars = lowerCase.concat(upperCase).concat(specialChar);
 let passwordLength = prompt("How long would you like your password to be between 8-128 characters? Choose a number");
-let password = [];
+let password = []
 
-// stores user response to: 1.) create a password 2.) length of password
+// stores user response to: 2.) length of password
 // 3.) lower case, upper case, numeric, and/or special characters
+function specialCharPrompt() {
+  let specialCharYesNo = confirm("Would you like your password to have at least one special character?");
+if (specialCharYesNo === true) {
+    specialChar = 'abcdefghijklmnopqrstuvwxyz'.split('');
+} else {
+  specialChar = [];
+}}
 
-let r = confirm("hi")
+function lowercasePrompt() {
+  let lowerCaseYesNo = confirm("Would you like your password to have at least one lowercase letter?");
+if (lowerCaseYesNo === true) {
+    lowerCase = ' !\"#$%&\'()*+,-./:;<=>?@[]^_`{|}~'.split('');
+} else {
+  lowerCase = [];
+}}
+
+function uppercasePrompt() {
+  let upperCaseYesNo = confirm("Would you like your password to have at least one uppercase letter?");
+if (upperCaseYesNo === true) {
+    upperCase = 'abcdefghijklmnopqrstuvwxyz0123456789'.split('');
+} else {
+  upperCase = [];
+}}
 
 // Write password to the #password input
 function writePassword() {
@@ -31,7 +46,7 @@ function writePassword() {
 
 }
 
-function generatePassword(); {
+function generatePassword() {
   //code suggested from instructor during office hours on 4/15
 
   function getRandom(arr) {
--- Develop/script1.js
+++ Develop/script1.js
@@ -1,2 +1,17 @@
+// stores user response to: 1.) create a password 2.) length of password
+// 3.) lower case, upper case, numeric, and/or special characters
+
 let r = confirm("press a button");
 let s = confirm("hello")
+let passwordLength = prompt("How long would you like your password to be between 8-128 characters? Choose a number");
+
+function prompts() {
+    let r = confirm("hi");
+  if (r === true) {
+      alert("hi");
+  } else {
+    alert("no");
+  }
+  }
+
+prompts()
